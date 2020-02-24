70b23af66385414fafe04fe3433f20fd5a9f185c
CrazyCoder009 <41443510+CrazyCoder009@users.noreply.github.com>
2020-02-24 17:34:14 +0530

    Autolocking made little better by adding sleep timer and removed face capture screen, Still needs development..

--- pyautolock.py
+++ pyautolock.py
@@ -11,7 +11,7 @@ cap = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while(True):
-	
+	time.sleep(10)
 	ret, frame = cap.read()
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -26,34 +26,38 @@ while(True):
 
 	#print("Found {0} faces!".format(len(faces)))
 	#print("Checking for User Presence...")
-	#time.sleep(3)
+	
 	
 	#First Check for face 
 	if(len(faces)==0):	
-		print("Detecting no User, waiting for 5 sec...")
-		time.sleep(5)
-		
-	# Second Check for face 
-	
-		if(len(faces)==0):
-			pyautogui.hotkey('win', 'r')
-			pyautogui.typewrite("cmd\n") 
-			sleep(0.500)    
-			pyautogui.typewrite("rundll32.exe user32.dll, LockWorkStation\n exit\n") 
-			break
+		print("Detecting no User, waiting for 2 sec...")
+		time.sleep(2)
+		for i in range(0,5):
+			# Second Check for face 
+			time.sleep(1)
+			if (len(faces)==0):
+				pyautogui.hotkey('win', 'r')
+				pyautogui.typewrite("cmd\n") 
+				sleep(0.500)    
+				pyautogui.typewrite("rundll32.exe user32.dll, LockWorkStation\n exit\n") 
+				break
+			else: 
+				print("User Detected, resuming monitoring...")
+				break
+			
 		break	
 	
 	
 	# Rectangle around the faces
-	for (x, y, w, h) in faces:
+	'''for (x, y, w, h) in faces:
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+'''
 
 	# Display the resulting frame
-	cv2.imshow('frame', frame)
+	'''cv2.imshow('frame', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
-						
+		'''				
 cap.release()
 cv2.destroyAllWindows()
 
