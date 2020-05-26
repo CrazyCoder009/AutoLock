c3a9048102ea99d255e1cdf1b6817e85c63e7d05
CrazyCoder009 <nairrohit3@gmail.com>
2020-05-26 20:14:18 +0530

    Altered the code with Flags

--- pyautolock.py
+++ pyautolock.py
@@ -9,40 +9,28 @@ from time import sleep
 
 cap = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
-
+global flag=1
 def Check_Face():
 	while(True):
 		ret, frame = cap.read()
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 		# Detect faces in the image
-		faces = faceCascade.detectMultiScale(
+		global faces = faceCascade.detectMultiScale(
 			gray,
 			scaleFactor=1.1,
 			minNeighbors=5,
 			minSize=(30, 30)
 			#flags = cv2.CV_HAAR_SCALE_IMAGE
 		)
-
+		
 		print("Found {0} faces!".format(len(faces)))
 		print("Checking for User Presence...")
-		
-		#First Check for face 
-		if(len(faces)==0):	
-			print("Detecting no User, waiting for 2 sec...")
-			time.sleep(5)
-			if (len(faces)==0):
-				pyautogui.hotkey('win', 'r')
-				pyautogui.typewrite("cmd\n") 
-				sleep(0.500)    
-				pyautogui.typewrite("rundll32.exe user32.dll, LockWorkStation\n exit\n") 
-				break
-			else: 
-				print("User Detected, resuming monitoring...")
-				break
-			
-		
+		if (len(faces)==0):
+			print("Detecting no User! Flag set to 0")
+			flag=0
+			return flag
+
 		# Rectangle around the faces
 		'''for (x, y, w, h) in faces:
 			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
@@ -56,9 +44,31 @@ def Check_Face():
 	cap.release()
 	cv2.destroyAllWindows()
 
+def Execute_Lock():
+			if (len(faces)==0):
+				pyautogui.hotkey('win', 'r')
+				pyautogui.typewrite("cmd\n") 
+				sleep(0.500)    
+				pyautogui.typewrite("rundll32.exe user32.dll, LockWorkStation\n exit\n") 
+				break
+			else: 
+				print("Execute lock detected User, resuming monitoring...")
+				break
+
 while(True):
 	Check_Face()
 	time.sleep(10)
+	if flag==0:
+		#Second Check before trigger
+		print("Execute Lock Detecting no User, waiting for 5 sec...")
+		time.sleep(5)
+		if flag==0:
+		Execute_Lock()
+		else:
+			flag=1
+		break
+	break
+
 #splash plymouth.ignore-serial-consoles quiet
 
 	
