659cc1dd184d080c41f9d47b942a1d79911aebf6
CrazyCoder009 <nairrohit3@gmail.com>
2020-05-26 19:59:18 +0530

    Reformatted the code with proper function and testing out..

--- pyautolock.py
+++ pyautolock.py
@@ -10,31 +10,28 @@ from time import sleep
 cap = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-while(True):
-	time.sleep(10)
-	ret, frame = cap.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	# Detect faces in the image
-	faces = faceCascade.detectMultiScale(
-		gray,
-		scaleFactor=1.1,
-		minNeighbors=5,
-		minSize=(30, 30)
-		#flags = cv2.CV_HAAR_SCALE_IMAGE
-	)
+def Check_Face():
+	while(True):
+		ret, frame = cap.read()
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	#print("Found {0} faces!".format(len(faces)))
-	#print("Checking for User Presence...")
-	
-	
-	#First Check for face 
-	if(len(faces)==0):	
-		print("Detecting no User, waiting for 2 sec...")
-		time.sleep(2)
-		for i in range(0,5):
-			# Second Check for face 
-			time.sleep(1)
+		# Detect faces in the image
+		faces = faceCascade.detectMultiScale(
+			gray,
+			scaleFactor=1.1,
+			minNeighbors=5,
+			minSize=(30, 30)
+			#flags = cv2.CV_HAAR_SCALE_IMAGE
+		)
+
+		print("Found {0} faces!".format(len(faces)))
+		print("Checking for User Presence...")
+		
+		#First Check for face 
+		if(len(faces)==0):	
+			print("Detecting no User, waiting for 2 sec...")
+			time.sleep(5)
 			if (len(faces)==0):
 				pyautogui.hotkey('win', 'r')
 				pyautogui.typewrite("cmd\n") 
@@ -45,22 +42,23 @@ while(True):
 				print("User Detected, resuming monitoring...")
 				break
 			
-		break	
-	
-	
-	# Rectangle around the faces
-	'''for (x, y, w, h) in faces:
-		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-'''
+		
+		# Rectangle around the faces
+		'''for (x, y, w, h) in faces:
+			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	'''
 
-	# Display the resulting frame
-	'''cv2.imshow('frame', frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-		'''				
-cap.release()
-cv2.destroyAllWindows()
+		# Display the resulting frame
+		'''cv2.imshow('frame', frame)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+			'''				
+	cap.release()
+	cv2.destroyAllWindows()
 
+while(True):
+	Check_Face()
+	time.sleep(10)
 #splash plymouth.ignore-serial-consoles quiet
 
 	
